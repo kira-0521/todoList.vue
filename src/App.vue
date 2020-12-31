c1459dec75d038031ca66e2d9ec0b32b6f08a0ae
Kira Tanaka <kiratanaka0521@gmail.com>
2021-01-01 08:20:26 +0900

    component maintenance

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <mytodo></mytodo>
+    <newtodo></newtodo>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import mytodo from "./components/mytodo";
+import newtodo from "./components/newtodo";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
+    mytodo,
+    newtodo
+  },
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/mytodo.vue
@@ -0,0 +1,27 @@
+<template>
+  <tbody>
+      <!-- ここに <tr> で ToDo の要素を1行づつ繰り返し表示 -->
+      <tr>
+          <th>{{ item.id }}</th>
+          <td>{{ item.comment }}</td>
+          <td class="state">
+              <!-- 状態変更ボタンのボタン -->
+              <button>{{ item.state }}</button>
+          </td>
+          <td class="button">
+              <!-- 削除ボタンのボタン -->
+              <button>削除</button>
+          </td>
+      </tr>
+  </tbody>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
